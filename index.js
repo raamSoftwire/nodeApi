c986cc71e85e863dd1e7ff0d08c5f6e88568c128
Raam <raam.chauhan@softwire.com>
2020-04-09 08:17:57 +0100

    Add swagger-ui docs

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const swaggerUi = require('swagger-ui-express'), swaggerDocument = require('./swagger.json');
+
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
