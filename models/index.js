48cb68cf7c0e50aec73012f3abfc4e67bd395215
Raam <raam.chauhan@softwire.com>
2020-04-24 08:47:04 +0100

    Remove sync for sequelize TEST db

--- models/index.js
+++ models/index.js
@@ -12,7 +12,6 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-sequelize.sync();
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
