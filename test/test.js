4f4426a9f61460582df74c2c0985bc70f01856dd
Raam <raam.chauhan@softwire.com>
2020-04-27 08:32:03 +0100

    Update user tests

--- test/test.js
+++ test/test.js
@@ -9,6 +9,10 @@ const db = require('../models');
 
 chai.use(chaiHttp);
 
+// SQLite keeps track of the largest ROWID that a table has ever held using the special SQLITE_SEQUENCE table.
+// This means that even if we destroy the data and reset the primary key with restartIdentity, the rowIds do not reset
+// Therefore each test in this file adds to the DB and potentially affects the following tests
+
 describe('Users', () => {
 
     describe('GET /users/', () => {
@@ -19,18 +23,19 @@ describe('Users', () => {
             chai.request(app).get('/users/')
                 .end( (err, res) => {
                     expect(res.body).to.have.lengthOf(2);
+                    expect(res.body[0].name).to.equal("Joe")
+                    expect(res.body[1].name).to.equal("Jane")
                     expect(res).to.have.status(200);
                 })
-        })
+        });
     });
 
     describe('GET /users/:id', () => {
         it('should return user if id is found', async () => {
-            await db.user.create({name: "Joe"});
 
-            chai.request(app).get('/users/1')
+            chai.request(app).get('/users/2')
                 .end( (err, res) => {
-                    expect(res.body.name).to.equal("Joe");
+                    expect(res.body.name).to.equal("Jane");
                     expect(res).to.have.status(200);
                 })
         });
@@ -52,13 +57,13 @@ describe('Users', () => {
 
     describe('POST /users/', () => {
         it('should create user if correct content is given', async () => {
-            const newUser = {name: "Jane Doe"};
+            const newUser = {name: "Alice"};
 
             chai.request(app).post('/users/')
                 .send(newUser)
                 .end( (err, res) => {
-                    expect(res).to.have.status(201);
                     expect(res.body).to.include({name: newUser.name});
+                    expect(res).to.have.status(201);
                 })
         });
 
@@ -80,9 +85,7 @@ describe('Users', () => {
 
     describe('PUT /users/:id', () => {
         it('should update user if correct content is given', async () => {
-            await db.user.create({name: "Joe"});
-
-            const updatedUser = {name: "Jane Doe"};
+            const updatedUser = {name: "Joe Bloggs"};
             chai.request(app).put('/users/1')
                 .send(updatedUser)
                 .end( (err, res) => {
@@ -100,17 +103,13 @@ describe('Users', () => {
         });
 
         it('should return 400 if content is not given', async () => {
-            await db.user.create({name: "Joe"});
-
-            chai.request(app).put('/users/1')
+            chai.request(app).put('/users/2')
                 .end( (err, res) => {
                     expect(res).to.have.status(400);
                 })
         });
 
         it('should return 400 if content does not contain a name', async () => {
-            await db.user.create({name: "Joe"});
-
             chai.request(app).put('/users/1')
                 .send({age: 21})
                 .end( (err, res) => {
@@ -121,8 +120,6 @@ describe('Users', () => {
 
     describe('DELETE /users/:id', () => {
         it('should delete user if user is found', async () => {
-            await db.user.create({name: "Joe"});
-
             chai.request(app).delete('/users/1')
                 .end( (err, res) => {
                     expect(res).to.have.status(204);
@@ -130,7 +127,7 @@ describe('Users', () => {
         });
 
         it('should return 404 if id is not found', async () => {
-            chai.request(app).delete('/users/66')
+            chai.request(app).delete('/users/1000')
                 .end( (err, res) => {
                     expect(res).to.have.status(404);
                 })
