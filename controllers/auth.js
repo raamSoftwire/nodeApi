7a6a632df365643581b0ccb6e1a794962463446f
Raam <raam.chauhan@softwire.com>
2020-05-02 08:44:42 +0100

    Add JWT authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,18 @@
 const db = require('../models')
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-const saltRounds = 10;
+const privateKey = 'secret'
+const saltRounds = 10
 const User = db.user
 
 exports.register = async (req, res) => {
-
     try {
         if (!req.body.name || !req.body.password) {
             res.status(400).send({message: 'Content can not be empty'})
         }
 
-        const hash = await bcrypt.hash(req.body.password, saltRounds);
+        const hash = await bcrypt.hash(req.body.password, saltRounds)
         const newUser = {name: req.body.name, password: hash}
         await User.create(newUser)
         res.status(201).send(newUser)
@@ -19,7 +20,6 @@ exports.register = async (req, res) => {
     catch {
         res.status(400).end({message: 'Something went wrong'})
     }
-
 }
 
 exports.signIn = async (req, res) => {
@@ -32,14 +32,14 @@ exports.signIn = async (req, res) => {
             res.status(401).send({message: 'Authentication failed. User not found'})
         }
 
-        const match = await bcrypt.compare(password, user.password);
+        const match = await bcrypt.compare(password, user.password)
 
         if (match) {
-            res.json({success: true, token: 'JWT here'})
+            const token = jwt.sign({ userId: user.id }, privateKey, { expiresIn: 60 * 15 })
+            res.json({success: true, token})
         } else {
             res.status(401).send({success: false, message: 'Authentication failed. Wrong password'})
         }
-
     }
     catch {
         res.status(400).end({message: 'Something went wrong'})
