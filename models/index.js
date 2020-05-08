357f18a129daae52d1d302456314e12224bbdcd4
Raam <raam.chauhan@softwire.com>
2020-05-08 16:53:18 +0100

    Simplify DB config logic

--- models/index.js
+++ models/index.js
@@ -6,11 +6,7 @@ const config = require(__dirname + '/../config/config.js')[env]
 const db = {}
 
 let sequelize
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config)
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config)
-}
+sequelize = new Sequelize(config.database, config.username, config.password, config)
 
 db.user = require('./user.js')(sequelize, Sequelize)
 db.book = require('./book.js')(sequelize, Sequelize)
