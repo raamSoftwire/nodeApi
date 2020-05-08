8dea40d3e60e170a0f14886cb10d745e4bf9f90f
Raam <raam.chauhan@softwire.com>
2020-05-08 10:57:48 +0100

    Move config from json to js

---
+++ config/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    development: {
+        username: "node_api_db",
+        password: "node_api_db",
+        database: "node_api_db",
+        host: "127.0.0.1",
+        dialect: "postgres"
+    },
+    test: {
+        dialect: "sqlite",
+        storage: ":memory:"
+    },
+    production: {
+        username: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DB_NAME,
+        host: process.env.RDS_HOSTNAME,
+        dialect: "postgres"
+    }
+};
--- models/index.js
+++ models/index.js
@@ -2,15 +2,11 @@
 
 const Sequelize = require('sequelize')
 const env = process.env.NODE_ENV || 'development'
-const config = require(__dirname + '/../config/config.json')[env]
+const config = require(__dirname + '/../config/config.js')[env]
 const db = {}
 
 let sequelize
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config)
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config)
-}
+sequelize = new Sequelize(config.database, config.username, config.password, config)
 
 db.user = require('./user.js')(sequelize, Sequelize)
 db.book = require('./book.js')(sequelize, Sequelize)
